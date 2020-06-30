060ba142bea4c15b2d6b401b887629755b9ce63a
ahmedmeshref <a.meshref@alustudent.com>
2020-07-01 01:24:16 +0200

    Set up a venv & connect to db

--- config.py
+++ config.py
@@ -1,13 +1,15 @@
 import os
-SECRET_KEY = os.urandom(32)
-# Grabs the folder where the script runs.
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-# Enable debug mode.
-DEBUG = True
-
-# Connect to the database
 
 
-# TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+class Config:
+    SECRET_KEY = os.urandom(32)
+    # Grabs the folder where the script runs.
+    basedir = os.path.abspath(os.path.dirname(__file__))
+
+    # Enable debug mode.
+    DEBUG = True
+
+    # Connect to the database.
+    SQLALCHEMY_DATABASE_URI = os.environ.get('POSTGRESQL') + "fyyur"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+

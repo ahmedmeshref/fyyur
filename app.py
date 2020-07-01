4fa54fa4a16df19434d27fd2fe8eb57f937cb979
ahmedmeshref <a.meshref@alustudent.com>
2020-07-01 04:09:01 +0200

    Run migration

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ from logging import Formatter, FileHandler
 from flask_wtf import Form
 from forms import *
 from config import Config
+from flask_migrate import Migrate
 
 # ----------------------------------------------------------------------------#
 # App Config.
@@ -22,6 +23,7 @@ app = Flask(__name__)
 moment = Moment(app)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 # TODO: connect to a local postgresql database

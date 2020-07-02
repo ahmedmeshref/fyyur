a0bf3343b52d88aadf83206ed1e554116fb8d539
ahmedmeshref <a.meshref@alustudent.com>
2020-07-02 20:33:52 +0200

    Add implementation of show all venues

--- app/routes.py
+++ app/routes.py
@@ -40,32 +40,16 @@ def index():
 
 @app.route('/venues')
 def venues():
-    # TODO: replace with real venues data.
-    #       num_shows should be aggregated based on number of upcoming shows per venue.
-    # data = [{
-    #     "city": "San Francisco",
-    #     "state": "CA",
-    #     "venues": [{
-    #         "id": 1,
-    #         "name": "The Musical Hop",
-    #         "num_upcoming_shows": 0,
-    #     }, {
-    #         "id": 3,
-    #         "name": "Park Square Live Music & Coffee",
-    #         "num_upcoming_shows": 1,
-    #     }]
-    # }, {
-    #     "city": "New York",
-    #     "state": "NY",
-    #     "venues": [{
-    #         "id": 2,
-    #         "name": "The Dueling Pianos Bar",
-    #         "num_upcoming_shows": 0,
-    #     }]
-    # }]
-    data = db.session.query(Venue.city).group_by(Venue.city, Venue.state).first()
-    # return render_template('pages/venues.html', areas=data)
-    return data
+    # query all venues
+    venues = db.session.query(Venue.id, Venue.name, Venue.city, Venue.state).all()
+    # distribute venues to areas where areas -> {("City_1", "State_1"): [list_of_venues], ...}
+    areas = {}
+    for venue in venues:
+        if (venue.city, venue.state) not in areas:
+            areas[(venue.city, venue.state)] = []
+        areas[(venue.city, venue.state)].append([venue.id, venue.name])
+
+    return render_template('pages/venues.html', areas=areas)
 
 
 @app.route('/venues/search', methods=['POST'])
